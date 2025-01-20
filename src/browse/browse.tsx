595a9c3e7ea9564d0482f090f89db9970787ea19
the-legendary-me <jackieejefferson@gmail.com>
2025-01-20 14:13:16 -0800

    Initial commit for my portfolio website

--- src/browse/browse.tsx
+++ src/browse/browse.tsx
@@ -17,7 +17,7 @@ const Browse: React.FC = () => {
       backgroundGif: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTZ5eWwwbjRpdWM1amxyd3VueHhteTVzajVjeGZtZGJ1dDc4MXMyNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9dg/16u7Ifl2T4zYfQ932F/giphy.gif" // Dark storm clouds
     },
     {
-      name: "developer",
+      name: "friend",
       image: greyImage,
       backgroundGif: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGNidDl5emZpejY2eGFxa2I4NW0zZGNpbWRlbnBrZ3N2dWhhbzM1MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TFPdmm3rdzeZ0kP3zG/giphy.gif" // Flickering neon lights
     },
